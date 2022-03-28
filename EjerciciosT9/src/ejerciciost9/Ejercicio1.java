ac13d381a80c589ec6ac7be59a8ddf9643fc7f84
chiarab22 <cbocc@myuax.com>
2022-03-28 17:16:32 +0200

    correcciones

---
+++ EjerciciosT9/src/ejerciciost9/Ejercicio1.java
@@ -0,0 +1,16 @@
+package ejerciciost9;
+
+import java.util.ArrayList;
+
+
+public class Ejercicio1 {
+    Bicicleta b1 = new Bicicleta("Lapierre", "speed 400", 1 , 27);
+    Bicicleta b2 = new Bicicleta("Btwin", "riverside 900", 0, 10);
+    Giroscopio g1 = new Giroscopio("Segway", "Nine", 0, 40);
+    Giroscopio g2 = new Giroscopio("Weebot", "Echo", 1, 35);
+    Segway s1 = new Segway("Immotion", "v8", 0, 40);
+    Segway s2 = new Segway("Segway", "Ninebot One E+", 0, 30);
+
+    ArrayList<Vehiculos> vehiculos = new ArrayList<>();
+    
+}
